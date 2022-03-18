a80dd4ebc17ee0d7014e15cdde714b756f1f27b3
wufeiwua <wufeiwua@126.com>
2022-03-18 17:38:11 +0800

    feat(list):  cycle linked list

--- list/cycle_linked_list.go
+++ list/cycle_linked_list.go
@@ -1,1 +1,89 @@
 package list
+
+import "errors"
+
+type CycleLinkedList struct {
+	head *DoublyNode
+	Size int
+}
+
+func NewCycleLinkedList() *CycleLinkedList {
+	return &CycleLinkedList{
+		head: nil,
+		Size: 0,
+	}
+}
+
+func (list *CycleLinkedList) Insert(value interface{}) {
+	node := &DoublyNode{
+		prev:  nil,
+		next:  nil,
+		Value: value,
+	}
+	if list.head == nil {
+		list.head = node
+		list.head.prev = node
+		list.head.next = node
+	} else {
+		// head prev is the last node
+		list.head.prev.next = node
+		node.prev = list.head.prev
+		node.next = list.head
+		// reset the last node
+		list.head.prev = node
+	}
+	list.Size++
+}
+
+func (list *CycleLinkedList) Delete(value interface{}) {
+	node, idx := list.head, 0
+	for node != nil && node.Value != value && idx < list.Size {
+		node = node.next
+		idx++
+	}
+	if node == nil {
+		// cannot find
+		// do nothing
+		return
+	} else if list.head == node {
+		// the node is first
+		// reset head
+		list.head = list.head.next
+	}
+	node.prev.next = node.next
+	node.next.prev = node.prev
+	list.Size--
+}
+
+func (list *CycleLinkedList) Get(index int) (interface{}, error) {
+	node, idx := list.head, 0
+	if index < 0 || index > list.Size-1 {
+		return nil, errors.New("out of index")
+	}
+	for node != nil && idx != index {
+		idx++
+		node = node.next
+	}
+	if node == nil {
+		return nil, errors.New("node is nil")
+	}
+	return node.Value, nil
+}
+
+func (list *CycleLinkedList) Find(value interface{}) (int, error) {
+	node, idx := list.head, 0
+	for node != nil && node.Value != value {
+		idx++
+		node = node.next
+
+		// on circle
+		if idx >= list.Size {
+			node = nil
+			break
+		}
+	}
+	if node == nil {
+		return -1, errors.New("cannot find it")
+	}
+	return idx, nil
+}
--- list/list_test.go
+++ list/list_test.go
@@ -95,5 +95,52 @@ func TestDoublyLinkedList(t *testing.T) {
 }
 
 func TestCycleLinkedList(t *testing.T) {
+	list := NewCycleLinkedList()
+	list.Insert(1)
+	list.Insert(2)
+	list.Insert(3)
+	list.Insert(4)
+	list.Insert(5)
+	list.Insert(6)
 
+	list.Delete(1)
+	list.Delete(6)
+	list.Delete(3)
+
+	size := list.Size
+	t.Log("the list size is ", size)
+	for i := 0; i < size; i++ {
+		value, err := list.Get(i)
+		if err == nil {
+			t.Log(value)
+		}
+	}
+
+	if re, err := list.Find(1); err == nil {
+		t.Log(re)
+	} else {
+		t.Log(err)
+	}
+
+	if re, err := list.Find(2); err == nil {
+		t.Log(re)
+	} else {
+		t.Log(err)
+	}
+
+	if re, err := list.Find(5); err == nil {
+		t.Log(re)
+	} else {
+		t.Log(err)
+	}
+	t.Log("revers print")
+
+	node := list.head.prev
+	for node != nil {
+		t.Log(node.Value)
+		node = node.prev
+		if node == list.head.prev {
+			break
+		}
+	}
 }
