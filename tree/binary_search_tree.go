4640a5e60720d4db5ad2cc6d922ad089a2b0b03a
wufeiwua <wufeiwua@126.com>
2022-03-19 18:49:16 +0800

    feat(tree): binary search tree delete method

--- tree/binary_search_tree.go
+++ tree/binary_search_tree.go
@@ -15,6 +15,10 @@ func (tree *BinarySearchTree) Insert(value int) {
 	tree.root = insertNode(value, tree.root)
 }
 
+func (tree *BinarySearchTree) Delete(value int) {
+	tree.root = deleteNode(value, tree.root)
+}
+
 func insertNode(value int, node *BinaryTreeNode) *BinaryTreeNode {
 	if node == nil {
 		node = &BinaryTreeNode{
@@ -28,4 +32,34 @@ func insertNode(value int, node *BinaryTreeNode) *BinaryTreeNode {
 		node.right = insertNode(value, node.right)
 	}
 	return node
+}
+
+func deleteNode(value int, node *BinaryTreeNode) *BinaryTreeNode {
+	if value < node.Value {
+		node.left = deleteNode(value, node.left)
+	} else if value > node.Value {
+		node.right = deleteNode(value, node.right)
+	} else {
+		// 有两个子节点
+		if node.left != nil && node.right != nil {
+			min := findMin(node.right)
+			node.Value = min.Value
+			node.right = deleteNode(min.Value, node.right)
+		} else if node.left == nil && node.right != nil {
+			return node.right
+		} else if node.left != nil && node.right == nil {
+			return node.left
+		} else {
+			return nil
+		}
+	}
+	return node
+}
+
+func findMin(node *BinaryTreeNode) *BinaryTreeNode {
+	min := node
+	for min.left != nil {
+		min = node.left
+	}
+	return min
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,8 +1,57 @@
 package tree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBinarySearchTree(t *testing.T) {
 	tree := NewBinarySearchTree()
+	tree.Insert(6)
+	tree.Insert(2)
 	tree.Insert(1)
+	tree.Insert(5)
+	tree.Insert(3)
+	tree.Insert(4)
+	tree.Insert(8)
+
+	tree.Delete(3)
+	tree.Delete(2)
+	tree.Delete(6)
+
+	preorder(tree.root)
+	t.Log("====preorder====")
+
+	inorder(tree.root)
+	t.Log("====inorder====")
+
+	postorder(tree.root)
+	t.Log("====postorder====")
+}
+
+func preorder(node *BinaryTreeNode) {
+	if node == nil {
+		return
+	}
+	fmt.Printf("%v,", node.Value)
+	preorder(node.left)
+	preorder(node.right)
+}
+
+func inorder(node *BinaryTreeNode) {
+	if node == nil {
+		return
+	}
+	inorder(node.left)
+	fmt.Printf("%v,", node.Value)
+	inorder(node.right)
+}
+
+func postorder(node *BinaryTreeNode) {
+	if node == nil {
+		return
+	}
+	postorder(node.left)
+	postorder(node.right)
+	fmt.Printf("%v,", node.Value)
 }
